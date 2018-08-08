84ee003c225087557220164a019df5519c6e1046
Morteza218 <morteza.es218@gmail.com>
2018-08-08 14:47:11 +0430

    install some package

---
+++ app/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const validator = require('express-validator');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const passport = require('passport');
+
+module.exports = class Application {
+    constructor() {
+        this.setupExpress();
+    }
+    setupExpress() {
+        const server = http.createServer(app);
+        server.listen(3000 , () => console.log('Listening on port 3000'));
+    }
+}
