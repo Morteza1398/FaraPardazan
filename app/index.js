240e708e12d51e02651cf4a26e4b674749369478
Morteza1398 <morteza.es218@gmail.com>
2018-08-08 20:55:06 +0430

    add structures and routes

--- app/index.js
+++ app/index.js
@@ -14,9 +14,14 @@ const passport = require('passport');
 module.exports = class Application {
     constructor() {
         this.setupExpress();
+        this.setRouters();
     }
     setupExpress() {
         const server = http.createServer(app);
         server.listen(3000 , () => console.log('Listening on port 3000'));
+    }
+    setRouters() {
+        app.use(require('app/routes/api'));
+        app.use(require('app/routes/web'));        
     }
 }
